a97118aacb099b56723e4750981a47da70bacf2e
esman artem <jesman@yandex.ru>
2022-09-09 23:27:29 +0200

    Cors

--- utils/cors.js
+++ utils/cors.js
@@ -1,9 +1,11 @@
 module.exports.allowedCors = [
   'https://cinemaclub.nomoredomains.sbs',
   'https://api.cinemaclub.nomoredomains.sbs',
+  'https://api.mes.to.nomoredomains.xyz',
+  'http://cinemaclub.nomoredomains.sbs',
+  'http://api.cinemaclub.nomoredomains.sbs',
+  'http://api.mes.to.nomoredomains.xyz',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3099',
-  'http://cinemaclub.nomoredomains.sbs',
-  'http://api.cinemaclub.nomoredomains.sbs',
 ];
